bf9cd6782bf24672ffe6a03410c33e031974d731
natrix_dev <discordlenko@gmail.com>
2022-08-01 17:35:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const Eris = require("eris");
 const keepAlive = require("./server");
 keepAlive();
 
-const bot = new Eris("YOUR DISCORD ACCOUNT TOKEN"); //your can put your token in a .env file to secure it
+const bot = new Eris(process.env.TOKEN); //your can put your token in a .env file to secure it
  
 bot.connect();
